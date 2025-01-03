b9f2efd7b5a743ee3ccb04110fce0eb4725855d4
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-01-03 20:24:57 +0530

    Initial commit for MJHandiCraft project

---
+++ MJHandiCraft/MJHandiCraft/MJHandiCraftApp.swift
@@ -0,0 +1,32 @@
+//
+//  MJHandiCraftApp.swift
+//  MJHandiCraft
+//
+//  Created by Mantosh Kumar on 03/01/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MJHandiCraftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MJHandiCraft/MJHandiCraftTests/MJHandiCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  MJHandiCraftTests.swift
+//  MJHandiCraftTests
+//
+//  Created by Mantosh Kumar on 03/01/25.
+//
+
+import Testing
+@testable import MJHandiCraft
+
+struct MJHandiCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
