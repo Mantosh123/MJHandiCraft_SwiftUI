581b9fe3b25ad20cd56601743d64e64433f99f7c
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-01-11 23:02:05 +0530

    Add product images into list

--- MJHandiCraft/MJHandiCraft/HomeView.swift
+++ MJHandiCraft/MJHandiCraft/HomeView.swift
@@ -11,10 +11,10 @@ struct HomeView: View {
     
     var productType = [
         
-        Product( id: UUID(), name: "Ganesh Ji", discription: "Ganesh Ji made by macram thread", photos: "ganeshji", price: 600.0),
-        Product( id: UUID(), name: "Mirror", discription: "Mirror holder made by macram thread", photos: "mirror", price: 600.0),
+        Product( id: UUID(), name: "Ganesh Ji", discription: "Ganesh Ji made by macram thread", photos: "ganeshJi", price: 600.0),
+        Product( id: UUID(), name: "Mirror", discription: "Mirror holder made by macram thread", photos: "mirrirholder", price: 600.0),
         Product( id: UUID(), name: "Mobile Holder", discription: "Mobile Holder made by macram thread", photos: "mobileholder", price: 600.0),
-        Product( id: UUID(), name: "Taddy Jhula", discription: "Taddy Juhla macram thread", photos: "TaddyJuhla", price: 600.0),
+        Product( id: UUID(), name: "Taddy Jhula", discription: "Taddy Juhla macram thread", photos: "taddyjhula", price: 600.0),
         Product( id: UUID(), name: "Mandla Art", discription: "Mandla Art discription", photos: "mandalArt", price: 600.0),
 
     ]
@@ -25,10 +25,16 @@ struct HomeView: View {
             List(productType) { productType in
                 HStack(alignment: .top) {
                     Image(productType.photos)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 70)
+                    
                     VStack(alignment: .leading) {
                         
                         Text(productType.name)
+                            .bold()
                         Text(productType.discription)
+                            
                     }
                 }
             }
