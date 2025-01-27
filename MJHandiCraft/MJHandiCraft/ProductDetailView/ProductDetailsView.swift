08e1ee3dcab31bfb49f18fd6742aaa943281263c
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-01-27 23:24:07 +0530

    Add Grid Item for cell

--- MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
+++ MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 
+
 struct ProductDetailsView: View {
     
+    
+    let columns: [GridItem] = [
+        GridItem (.flexible()),
+        GridItem (.flexible()),
+        GridItem (.flexible()),
+    ]
+    
+    let arrValue = Array(1...5)
+    
     var body: some View {
-        
-        Image("list.dash")
-    }
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(arrValue, id: \.self) { item in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.blue)
+                                .frame(height: 150)
+                            Text("\(item)")
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .bold()
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
     
 }
 
