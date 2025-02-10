8fbf0f3a81840c7b417f73767281c1463c7a469b
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-02-10 22:24:50 +0530

    Remove samperator

--- MJHandiCraft/MJHandiCraft/HomeView/HomeView.swift
+++ MJHandiCraft/MJHandiCraft/HomeView/HomeView.swift
@@ -33,6 +33,7 @@ struct HomeView: View {
                         .navigationTitle("Select Categories")
                         .background(.red)
                 }
+                .listRowSeparator(.hidden)
             }
         }
     }
