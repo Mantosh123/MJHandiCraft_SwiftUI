0c5df1be91c9a8bcc462e499744b1e8a904972d1
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-02-17 20:42:26 +0530

    Added image position

--- MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
+++ MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
@@ -12,15 +12,33 @@ struct ProductDetailsView: View {
     let imageName: String
     
     var body: some View {
-        VStack {
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
+        ZStack {
+                    // Background content (if needed)
+                    Color.white.ignoresSafeArea()
+                    
+                    // Image positioned at the top bezel
+                    Image(imageName) // Replace with your actual image asset name
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: UIScreen.main.bounds.width) // Full width of the screen
+                        .offset(y: -UIScreen.main.bounds.height/2) // Move the image upwards
+            
+//            Image(imageName)
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        
+                    VStack {
+                        Spacer()
+                        Text("Content Below the Image")
+                            .font(.title)
+                            .padding()
+                    }
+                }
+       
     }
 }
 
 #Preview {
-    ProductDetailsView(imageName: "")
+    ProductDetailsView(imageName: "ganeshJi")
 }
