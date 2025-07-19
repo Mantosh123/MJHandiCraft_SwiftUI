a277731389860c284ccd66d6bf5211fdcc43ef7b
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-07-19 18:18:41 +0530

    Updated price and its UI

--- MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
+++ MJHandiCraft/MJHandiCraft/ProductDetailView/ProductDetailsView.swift
@@ -13,24 +13,29 @@ struct ProductDetailsView: View {
     
     var body: some View {
         ZStack {
-                    // Background content (if needed)
-                    Color.white.ignoresSafeArea()
-                    
-                    // Image positioned at the top bezel
-                    Image(imageName) // Replace with your actual image asset name
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: UIScreen.main.bounds.width) // Full width of the screen
-                        .offset(y: -UIScreen.main.bounds.height/2) // Move the image upwards
-                                    
-                    VStack {
-                        Spacer()
-                        Text("Content Below the Image")
-                            .font(.title)
-                            .padding()
-                    }
-                }
-       
+            // Background content (if needed)
+            Color.white.ignoresSafeArea()
+            
+            // Image positioned at the top bezel
+            Image(imageName) // Replace with your actual image asset name
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width) // Full width of the screen
+                .offset(y: -100) // Move the image upwards
+            
+            VStack {
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("   Mirror Holder   ").font(.title)
+                    .background(in: .capsule, fillStyle: FillStyle())
+                Text("  Price: 500/-  ").font(.subheadline)
+                    .background(in: .capsule, fillStyle: FillStyle())
+                    .foregroundColor(.red)
+                Spacer()
+            }
+        }
+        
     }
 }
 
